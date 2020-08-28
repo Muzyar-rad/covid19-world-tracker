84fa60a976fab34b95aa091ae4240af88456c20a
Muzyar Rad <muzyar.rad@gmail.com>
2020-08-27 19:32:42 -0700

    css fixed

--- src/components/header.js
+++ src/components/header.js
@@ -6,31 +6,31 @@ const Header = () => {
   return (
     <div>
       <CardDeck>
-        <Card bg="secondaty">
+        <Card bg="secondary" text="white" style={{ margin: "10px" }}>
           <Card.Body>
             <Card.Title>Number of Cases</Card.Title>
             <Card.Text>20</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-white">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="success">
+        <Card bg="success" text="white" style={{ margin: "10px" }}>
           <Card.Body>
-            <Card.Title>Number of Deaths</Card.Title>
+            <Card.Title>Number of Recovered</Card.Title>
             <Card.Text>0</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-white">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="danger">
+        <Card bg="danger" text="white" style={{ margin: "10px" }}>
           <Card.Body>
-            <Card.Title>Number of Recovered</Card.Title>
+            <Card.Title>Number of Deaths</Card.Title>
             <Card.Text>20</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-white">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
       </CardDeck>
